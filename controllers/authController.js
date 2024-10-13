5ea6c6deb60a5aea598b38f2ed74cfb2331916ed
Rosemary Wangui <rosecikumary@gmail.com>
2024-10-14 01:04:21 +0300

    added explanation comments

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcryptjs');
 
 //user registration function
 exports.registerUser = async (request, response) => {
-    //fetch data
-    const { name, email, password } = request.body;
+    //fetching user data from the request body when the form is submitted. 
+    const { name, email, password } = request.body;// body is a property of the request and that's where we will fetch the data such as email...
     try{
         //check if user exists
         const [rows] = await db.execute('SELECT * FROM users WHERE email = ?', [email]);
@@ -34,13 +34,15 @@ exports.loginUser = async (request, response) => {
         if(rows.length === 0){
             return response.status(400).json({ message: 'User not found! Please register.'});
         }
-        const user = rows[0];
+        const user = rows[0];//if user exist we create a varible that will hold the records.it is holding the result of our query
 
         //compare the password
         const isMatch = await bcrypt.compare(password, user.password);
         if(!isMatch){
             return response.status(400).json({ message: 'Invalid credentials!' });
         }
+        //if password matches,we can store this to the session, by accessing the request then the session and store it under a user variable. 
+        // req.session.user = user
         response.status(200).json({ message: 'Login successful!', name: user.name, email : user.email });
     } catch(error) {
         response.status(500).json({ message: 'An error occured!', error });
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 //import
 const express = require('express');
-const path = require('path');
+const path = require('path');// capturing the location of your directories and files
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
 
 const app = express();
 
-//set-up middleware
+//set-up middleware(application/function/feature that sits between 2 interafces)
 app.use(express.static(path.join(__dirname, '/')));
-app.use(express.json());
+app.use(express.json());//using JSON data format in handling data
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRoutes)
-
+//routes for rendering the form
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, 'index.html'));
+    response.sendFile(path.join(__dirname, 'index.html'));// route for displaying the index form/webpage
 });
 
 app.get('/login', (request, response) => {
